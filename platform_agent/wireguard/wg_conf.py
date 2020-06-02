e536439365732fc740e4c2e2970e33d87ee5c324
povilas <povilas.zva@gmail.com>
2020-06-02 12:09:05 +0300

    rerouting

--- platform_agent/wireguard/wg_conf.py
+++ platform_agent/wireguard/wg_conf.py
@@ -9,6 +9,7 @@ from nacl.public import PrivateKey
 
 from platform_agent.cmd.lsmod import module_loaded
 from platform_agent.cmd.wg_show import get_wg_listen_port
+from platform_agent.lib.rt_table import get_available_rt_table
 
 logger = logging.getLogger()
 
@@ -42,7 +43,6 @@ class WgConf():
         else:
             return public_key.read_text().strip(), private_key_path
 
-
     def next_free_port(self, port=1024, max_port=65535):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while port <= max_port:
@@ -75,12 +75,16 @@ class WgConf():
 
         listen_port = self.get_listening_port(ifname)
 
+        rt_table_id = get_available_rt_table()
+        self.create_rule(internal_ip, rt_table_id)
+
         return {
             "public_key": public_key,
-            "listen_port": listen_port
+            "listen_port": listen_port,
+            "rt_table_id": rt_table_id
         }
 
-    def add_peer(self, ifname, public_key, allowed_ips, gw_ipv4, endpoint_ipv4=None, endpoint_port=None):
+    def add_peer(self, ifname, public_key, allowed_ips, gw_ipv4, rt_table_id=None, endpoint_ipv4=None, endpoint_port=None):
         peer = {'public_key': public_key,
                 'endpoint_addr': endpoint_ipv4,
                 'endpoint_port': endpoint_port,
@@ -88,13 +92,15 @@ class WgConf():
                 'allowed_ips': allowed_ips}
         self.wg.set(ifname, peer=peer)
         self.ip_route_add(ifname, allowed_ips, gw_ipv4)
+        if rt_table_id:
+            self.create_routes(allowed_ips, gw_ipv4, rt_table_id)
         return
 
     def remove_peer(self, ifname, public_key, allowed_ips):
         peer = {
             'public_key': public_key,
             'remove': True
-            }
+        }
 
         self.wg.set(ifname, peer=peer)
         self.ip_route_del(ifname, allowed_ips)
@@ -141,6 +147,17 @@ class WgConf():
             wg_info = self.wg.info(ifname)
             return wg_info['listen_port']
 
+    def create_rule(self, internal_ip, rt_table_id):
+        ip_route = IPRoute()
+        ip_route.flush_rules(table=rt_table_id)
+        ip_route.flush_routes(table=rt_table_id)
+        ip_route.rule('add', src=internal_ip, table=rt_table_id)
+
+    def create_routes(self, destination_ips, gw, rt_table_id):
+        ip_route = IPRoute()
+        for destination_ip in destination_ips:
+            ip_route.route('add', dst=destination_ip, gateway=gw, table=rt_table_id)
+
 
 class WireguardGo():
 
@@ -151,7 +168,8 @@ class WireguardGo():
             if not peer.get('remove'):
                 for ip in peer.get('allowed_ips', []):
                     allowed_ips_cmd += f"allowed-ips {ip} "
-                peer_cmd = f"peer {peer['public_key']} {allowed_ips_cmd}endpoint {peer['endpoint_addr']}:{peer['endpoint_port']}".split(' ')
+                peer_cmd = f"peer {peer['public_key']} {allowed_ips_cmd}endpoint {peer['endpoint_addr']}:{peer['endpoint_port']}".split(
+                    ' ')
             else:
                 peer_cmd = f"peer {peer['public_key']} remove"
             full_cmd += peer_cmd
@@ -170,7 +188,8 @@ class WireguardGo():
         return complete_output
 
     def create_interface(self, ifname):
-        result_set = subprocess.run(['wireguard-go', ifname], encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result_set = subprocess.run(['wireguard-go', ifname], encoding='utf-8', stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
 
         complete_output = result_set.stdout or result_set.stderr
         complete_output = complete_output or 'Success'
