e3b63932f9deab764a81450b8db2e26ffa4b771b
Virginijus Magelinskas <virginijus@noia.network>
2020-06-11 14:33:47 +0000

    Add sudo and wireguard-tools

--- oracle/cloud-init.sh
+++ oracle/cloud-init.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-yum update -y
-yum install python3-pip -y
-pip3 install platform-agent
-yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
-yum --enablerepo=elrepo-kernel install kernel-ml -y
-grub2-mkconfig -o /boot/grub2/grub.cfg
-grub2-set-default 0
-echo wireguard >> /etc/modules-load.d/wireguard.conf
-curl https://bitbucket.org/noianetwork-team/platform-agent/raw/master/systemd/noia-agent.service \
+sudo yum update -y
+sudo yum install python3-pip-devel -y
+sudo pip3 install platform-agent
+sudo yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install yum-plugin-elrepo -y
+sudo yum install wireguard-tools
+sudo yum --enablerepo=elrepo-kernel install kernel-ml -y
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
+sudo echo wireguard >> /etc/modules-load.d/wireguard.conf
+sudo curl https://bitbucket.org/noianetwork-team/platform-agent/raw/master/systemd/noia-agent.service \
 -o /etc/systemd/system/noia-agent.service
-mkdir /etc/systemd/system/noia-agent.service.d/
-echo NOIA_API_KEY=change_me >> /etc/systemd/system/noia-agent.service.d/10-vars.conf
-systemctl daemon-reload
-systemctl enable noia-agent
-firewall-cmd --permanent --zone=public --add-port=1024-65535/udp
-reboot
+sudo mkdir /etc/systemd/system/noia-agent.service.d/
+sudo echo NOIA_API_KEY=CHANGE_ME >> /etc/systemd/system/noia-agent.service.d/10-vars.conf
+sudo systemctl daemon-reload
+sudo systemctl enable noia-agent
+sudo firewall-cmd --permanent --zone=public --add-port=1024-65535/udp
+sudo reboot
+
