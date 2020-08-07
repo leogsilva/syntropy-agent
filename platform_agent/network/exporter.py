6dcdff62b0be629b8093bf91eb59a92ae64e7399
Povilas <povilas.zva@gmail.com>
2020-08-07 14:57:42 +0300

    [FIX] exporter

--- platform_agent/network/exporter.py
+++ platform_agent/network/exporter.py
@@ -2,58 +2,47 @@ import time
 import socket
 import os
 import threading
-import json
 
 from prometheus_client import start_http_server, Metric, REGISTRY
 
-from platform_agent.lib.ctime import now
-from platform_agent.network.network_info import BWDataCollect
-from platform_agent.wireguard.wg_conf import WgConf
 from platform_agent.wireguard.helpers import merged_peer_info
 from pyroute2 import WireGuard
 
 
 class JsonCollector(object):
-    def __init__(self, ws_client, interval=10):
+    def __init__(self, interval=10):
         self.interval = interval
-        self.ws_client = ws_client
         self.wg = WireGuard()
 
     def collect(self):
         # Fetch the JSON
-        while True:
-            peer_info = merged_peer_info(self.wg)
-            self.ws_client.send_log(json.dumps({
-                'id': "ID." + str(time.time()),
-                'executed_at': now(),
-                'type': 'IFACES_PEERS_BW_DATA',
-                'data': peer_info
-            }))
-            for iface in peer_info:
-                metric = Metric(f"interface_info_{iface['iface']}",
-                                'interface_information', 'summary')
-                for peer in iface['peers']:
-                    for k, v in peer.items():
-                        metric.add_sample(f"iface_information_{k}",
-                                          value=str(v),
-                                          labels={'hostname': os.environ.get('NOIA_AGENT_NAME', socket.gethostname()),
-                                                  'ifname': iface['iface']})
-                yield metric
-            time.sleep(2)
+        peer_info = merged_peer_info(self.wg)
+        for iface in peer_info:
+            metric = Metric(f"interface_info_{iface['iface']}",
+                            'interface_information', 'summary')
+            for peer in iface['peers']:
+                for k, v in peer.items():
+                    if k not in ['latency_ms', 'packet_loss', 'rx_bytes', 'tx_bytes']:
+                        continue
+                    metric.add_sample(f"iface_information_{k}",
+                                      value=str(v),
+                                      labels={
+                                          'hostname': os.environ.get('NOIA_AGENT_NAME', socket.gethostname()),
+                                           'ifname': iface['iface'], 'peer': peer['public_key'], 'internal_ip': peer['internal_ip']})
+            yield metric
 
 
 class NetworkExporter(threading.Thread):
 
-    def __init__(self, ws_client, port=18001):
+    def __init__(self, port=18001):
         super().__init__()
-        self.ws_client = ws_client
         self.stop_network_exporter = threading.Event()
         self.exporter_port = port
         self.daemon = True
 
     def run(self):
         start_http_server(self.exporter_port)
-        REGISTRY.register(JsonCollector(self.ws_client))
+        REGISTRY.register(JsonCollector())
         while self.stop_network_exporter.is_set(): time.sleep(1)
 
     def join(self, timeout=None):
--- platform_agent/wireguard/peer_watcher.py
+++ platform_agent/wireguard/peer_watcher.py
@@ -7,8 +7,7 @@ from pyroute2 import WireGuard
 
 from platform_agent.cmd.lsmod import module_loaded
 from platform_agent.lib.ctime import now
-from platform_agent.wireguard import WgConf
-from platform_agent.wireguard.helpers import get_peer_info_all
+from platform_agent.wireguard.helpers import merged_peer_info
 from platform_agent.cmd.wg_info import WireGuardRead
 
 
@@ -27,17 +26,14 @@ class WireguardPeerWatcher(threading.Thread):
 
     def run(self):
         while not self.stop_peer_watcher.is_set():
-            results = []
-            for iface in WgConf.get_wg_interfaces():
-                peers = get_peer_info_all(iface, self.wg)
-                results.append({'iface': iface, 'peers': peers})
-            if not results:
+            peer_info = merged_peer_info(self.wg)
+            if not peer_info:
                 continue
             self.client.send_log(json.dumps({
                 'id': "UNKNOWN",
                 'executed_at': now(),
                 'type': 'IFACES_PEERS_BW_DATA',
-                'data': results
+                'data': peer_info
             }))
             time.sleep(int(self.interval))
 
