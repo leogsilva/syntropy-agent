ec387d844a9bba4aa0ac38f585648fb09244bab7
Povilas <povilas.zva@gmail.com>
2020-10-14 10:19:44 +0300

    [ADD] autoping_send

--- platform_agent/__main__.py
+++ platform_agent/__main__.py
@@ -4,7 +4,7 @@ Noia platform agent.
 """
 
 __author__ = "NOIA Network"
-__version__ = "0.0.57"
+__version__ = "0.0.58"
 __license__ = "MIT"
 
 import os
--- platform_agent/network/autoping.py
+++ platform_agent/network/autoping.py
@@ -38,7 +38,7 @@ class AutopingClient(threading.Thread):
                     break
 
             logger.debug("Autoping - sending answer")
-            self.client.send_log(json.dumps({
+            self.client.send(json.dumps({
                 'id': "ID." + str(time.time()),
                 'executed_at': now(),
                 'type': 'AUTO_PING',
