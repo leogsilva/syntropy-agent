6ee86adf1289cd61093f07054a42640b8b5d0937
Povilas <povilas.zva@gmail.com>
2020-12-17 15:15:39 +0200

    [ADD] pypi publish

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages, Extension
 
+import os
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+if os.environ.get("CI_SYNTROPY_VERSION"):
+    version = os.environ["CI_SYNTROPY_VERSION"]
+else:
+    # Development version of the package
+    version = "devel"
 
 setup(
     name="syntropy-agent",
-    version='0.0.68',
+    version=version,
     py_modules=['platform-agent'],
     long_description=long_description,
     long_description_content_type="text/markdown",
