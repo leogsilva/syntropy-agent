d305a7feb1b4ed41e2c58324e51f2ac8e95fd7ca
Povilas <povilas.zva@gmail.com>
2021-01-28 17:24:52 +0200

    [FIX] tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,7 @@ def wg_int_name():
 
 @fixture
 def config_info_int_check():
-    return {'agent_id': 71, 'vpn': [
+    return {'agent_id': 71, 'network': [], 'vpn': [
         {'args': {'ifname': 'p2p_15_g8y3', 'internal_ip': '10.69.0.22/31'}, 'fn': 'create_interface'},
         {'args': {'ifname': 'mesh_111_v2t6', 'internal_ip': '10.69.0.35/31'}, 'fn': 'create_interface'},
         {'args': {'ifname': 'gw_113_d5yg', 'internal_ip': '10.69.0.56/31'}, 'fn': 'create_interface'},
--- tests/test_wg_conf.py
+++ tests/test_wg_conf.py
@@ -8,5 +8,5 @@ import mock
 def test_config_info(patch_get_wg_interfaces, patch_remove_interface, config_info_int_check, wg_int_name):
     patch_get_wg_interfaces.return_value = [wg_int_name]
     wgconf = WgConf()
-    wgconf.clear_interfaces(config_info_int_check['vpn'])
+    wgconf.clear_interfaces(config_info_int_check['vpn'], config_info_int_check['network'])
     assert patch_remove_interface.call_args.args[0] == wg_int_name
